c673bff39a9cb0f957949badb3339bd58aeecf4a
Raúl Ramón Gracia <651849@unizar.es>
2020-11-08 20:47:48 +0100

    Added rel="noreferrer" to links

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <footer className="Footer">
-            Coded by <a href="https://www.linkedin.com/in/ratcliffealison/" target="_blank">Alison Ratcliffe</a> and open sourced<a href="https://github.com/ratcliffealison/weather-app" target="_blank"> on GitHub</a>
+            Coded by <a href="https://www.linkedin.com/in/ratcliffealison/" target="_blank" rel="noreferrer">Alison Ratcliffe</a> and open sourced<a href="https://github.com/ratcliffealison/weather-app" target="_blank" rel="noreferrer"> on GitHub</a>
         </footer>
     )
 }
